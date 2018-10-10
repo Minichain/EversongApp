9f4dd1a87cc3b1138bcca30fe16e9cef89c83189
Minichain <adriacadenarubio@gmail.com>
2018-10-10 21:40:22 +0200

    log-lib added back

--- app/src/main/cpp/ProcessAudio.h
+++ app/src/main/cpp/ProcessAudio.h
@@ -2,6 +2,13 @@
 #define EVERSONGAPP_PROCESSAUDIO_H
 
 #include <math.h>
+#include <android/log.h>
+
+#define  LOG_TAG    "EversongApp"
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define  LOGW(...)  __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
+#define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 
 class ProcessAudio {
 public:
@@ -10,6 +17,5 @@ public:
     static double* fft(double* inputReal, double* inputImag, int length, bool DIRECT);
     static int bitReverseReference(int j, int nu);
 };
-
 
 #endif //EVERSONGAPP_PROCESSAUDIO_H
