b0f73179eb3018e2638760096319639d3a827028
Minichain <adriacadenarubio@gmail.com>
2019-01-19 15:39:48 +0100

    24 fps are too much for some smartphone devices, it's been set to 10 fps

--- app/src/main/java/com/upf/minichain/eversongapp/Parameters.java
+++ app/src/main/java/com/upf/minichain/eversongapp/Parameters.java
@@ -23,7 +23,7 @@ public class Parameters {
     public static final int SAMPLE_RATE = 44100;    // The sampling rate (16000, 22050, 44100)
     public static final int BUFFER_SIZE = 8192;     // It must be a power of 2 (2048, 4096, 8192, 16384...)
     public static final int HOP_SIZE = 2048;        // It must be a power of 2
-    public static final int FRAMES_PER_SECOND = 24;
+    public static final int FRAMES_PER_SECOND = 10;
 
     /************
      * Variables
